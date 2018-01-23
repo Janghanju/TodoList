16cd7238a4af81230196a4ff873414a56d3efd5b
Janghanju <hanju1215@naver.com>
2018-01-23 16:21:23 +0900

    first commit

--- app/src/main/java/com/asuscom/janghanju/myapp/todolist/Naver/Naver_Secreat.java
+++
@@ -1,9 +0,0 @@
-package com.asuscom.janghanju.myapp.todolist.Naver;
-
-/**
- * Created by MacBook on 2018. 1. 23..
- */
-
-public class Naver_Secreat {
-    public final String CLIEMT_ID = "YOUR NAVER CLIENT ID";
-}
